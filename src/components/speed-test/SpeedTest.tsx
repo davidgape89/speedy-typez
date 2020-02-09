d590a62c1f294629b2856ee6305e6f5789af78b9
David Garcia Perez <david@localhost.localdomain>
2020-02-09 12:17:40 +0100

    Implemented basic functionality

---
+++ src/components/speed-test/SpeedTest.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useRef } from 'react';
+import {CountDownTimer, TimerRef} from '../timer/Timer';
+import {englishText} from './words';
+import './SpeedTest.scss';
+
+const SpeedTest = () => {
+  const [input, setInput] = React.useState<string>('');
+  const [isRunning, setIsRunning] = React.useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = React.useState<boolean>(false);
+  const [inputStack, setInputStack] = React.useState<string[]>([]);
+  const timerRef = useRef<TimerRef>();
+  const words = englishText.replace(/\n/g, '').split(' ');
+
+  useEffect(() => {
+    if(input.length > 0) {
+      setIsRunning(true);
+    }
+  }, [input]);
+
+  function reset() {
+    setInput('');
+    setInputStack([]);
+    setIsDisabled(false);
+    setIsRunning(false);
+    if(timerRef && timerRef.current) timerRef.current.reset();
+  }
+
+  function handleTimeOver() {
+    setIsDisabled(true);
+  }
+
+  function handleTicker() {
+    setIsRunning(!isRunning);
+  }
+
+  function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+    switch(e.keyCode){
+      case 32:
+        e.preventDefault();
+        setInputStack([...inputStack, input]);
+        setInput('');
+        break;
+      case 8:
+        if(inputStack.length > 0 && !input.length) {
+          e.preventDefault();
+          const lastIndex = inputStack.length - 1;
+          setInput(inputStack[lastIndex]);
+          setInputStack(inputStack.slice(0, lastIndex))
+        }
+        break;
+      case 27:
+        reset();
+        break;
+    }
+  }
+
+  function getWordStatus(i: number) {
+    if (inputStack[i]) {
+      if(words[i] === inputStack[i]) return "is-valid";
+      else return "is-invalid";
+    } else if (inputStack.length === i) {
+      if(words[i].startsWith(input)) return "is-active";
+      else return "is-invalid";
+    }
+  }
+
+  return (
+    <div className="speed-test">
+      <div className="speed-test__text">
+        {words.map((word, i) => (
+          <React.Fragment key={i}>
+            <span
+              className={getWordStatus(i)}>
+              {word}
+            </span>
+            <span>{' '}</span>
+          </React.Fragment>
+        ))}
+      </div>
+      <input
+        type="text"
+        className="speed-test__input"
+        disabled={isDisabled}
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        onKeyDown={handleKeyPress}
+      />
+      <CountDownTimer
+        ref={timerRef}
+        initTime={60 * 1}
+        isRunning={isRunning}
+        onTimeOver={handleTimeOver}
+      />
+      <button onClick={handleTicker}>Start/stop</button>
+      {isDisabled && <div>
+        Your word count is {inputStack.length / 2}!
+      </div>}
+    </div>
+  )
+}
+
+export default SpeedTest;
---
+++ src/components/timer/Timer.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
+
+export enum TimerDirection {
+  ASC = 1,
+  DESC = -1,
+}
+
+interface TimerProps {
+  initTime: number;
+  onTimeOver: () => void;
+  isRunning?: boolean;
+  direction?: TimerDirection;
+}
+
+export interface TimerRef {
+  reset: () => void;
+}
+
+export const CountDownTimer = forwardRef(({
+    isRunning = true,
+    initTime,
+    direction = TimerDirection.DESC,
+    onTimeOver
+  }: TimerProps, ref) => {
+  const [time, setTime] = useState(initTime);
+  const [timeoutRef, setTimeoutRef] = useState();
+  const minutes = Math.floor(time / 60).toString();
+  const seconds = (time % 60).toString();
+
+  if ( isRunning && !timeoutRef) {
+    if (time === 0) {
+      onTimeOver();
+    } else {
+      setTimeoutRef(
+        setTimeout(() => {
+          setTime(time => time + direction)
+          setTimeoutRef(null);
+        }, 1000)
+      );
+    }
+  }
+
+  useImperativeHandle(ref, () => ({
+    reset() {
+      setTime(initTime);
+    }
+  }));
+
+  useEffect(() => {
+    if (!isRunning && timeoutRef) {
+      clearTimeout(timeoutRef);
+      setTimeoutRef(null);
+    }
+  }, [isRunning, timeoutRef]);
+
+  return (
+    <span>
+      {minutes.length === 1 ? `0${minutes}`: minutes}:
+      {seconds.length === 1 ? `0${seconds}`: seconds}
+    </span>
+  )
+})
