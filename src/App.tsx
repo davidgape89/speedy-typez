992e4bd6367500e1251b343ee1a3b73fd007a26f
David Garcia Perez <david@localhost.localdomain>
2020-02-28 18:16:16 +0100

    Some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.scss';
 import SpeedTest from './components/speed-test/SpeedTest';
+import Header from './components/header/Header';
+import Keyboard from './components/keyboard/Keyboard';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <SpeedTest />
+      <Keyboard />
     </div>
   );
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Speedy Typez</h1>
+    </header>
+  )
+}
