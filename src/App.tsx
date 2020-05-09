f11cdaa5cda3984a1e99db724af29ceeb4828a5c
David Garcia Perez <david@localhost.localdomain>
2020-05-09 20:17:49 +0200

    feat: implemented keyboard mapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SpeedTest from './components/speed-test/SpeedTest';
 import Header from './components/header/Header';
 import Keyboard from './components/keyboard/Keyboard';
+import KeyboardMapper, { KeyMap } from './components/key-mapper/KeyMapper';
 import './App.scss';
 
 const App = () => {
@@ -9,7 +10,11 @@ const App = () => {
     <div className="App">
       <Header />
       <SpeedTest />
-      <Keyboard />
+      <KeyboardMapper>
+        {(keys: KeyMap) => (
+          <Keyboard keys={keys} />
+        )}
+      </KeyboardMapper>
     </div>
   );
 }
